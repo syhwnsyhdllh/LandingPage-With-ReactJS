7ffd931e02e9f8d9bb64b59d55d91e66214fa2a5
Syahwan Syahadillah Hamdih <cl09ay@gmail.com>
2024-11-19 17:07:04 +0800

    Benefit,Collaboration-section-finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
 import Button from "./components/Button";
+import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 
@@ -9,6 +11,8 @@ function App() {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
+        <Benefits />
+        <Collaboration />
       </div>
       <ButtonGradient />
     </>
